04df67e181df1e0c2f2b810a479b5d7e51ce8593
Willi Meierhof <willi@meierhof.dev>
2023-08-27 19:01:41 +0200

    ci: Allow sentance case for subject-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,6 @@ module.exports = {
     "header-max-length": [0, "always", Infinity],
     "body-max-line-length": [0, "always", Infinity],
     "footer-max-line-length": [0, "always", Infinity],
-    "subject-case": ["sentence-case"],
+    "subject-case": [0, "never", ["sentence-case"]],
   },
 };
