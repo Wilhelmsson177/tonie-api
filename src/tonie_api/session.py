8b0a09e18d93636bbee0f132f483a076e7850909
Tom Freudenberg <tom.freudenberg@4commerce.de>
2024-01-31 18:32:55 +0100

    add user_agent identification to API requests

--- src/tonie_api/session.py
+++ src/tonie_api/session.py
@@ -13,16 +13,16 @@ class TonieCloudSession(requests.Session):
         super().__init__()
         self.token: None | str = None
 
-    def acquire_token(self, username: str, password: str) -> None:
+    def acquire_token(self, username: str, password: str, user_agent: str) -> None:
         """Acquire the token from the ToniCloud SSO login using username and password.
 
         Args:
             username (str): The username
             password (str): The password_
         """
-        self.token = self._acquire_token(username, password)
+        self.token = self._acquire_token(username, password, user_agent)
 
-    def _acquire_token(self, username: str, password: str) -> str:
+    def _acquire_token(self, username: str, password: str, user_agent: str) -> str:
         data = {
             "grant_type": "password",
             "client_id": "my-tonies",
@@ -30,5 +30,9 @@ class TonieCloudSession(requests.Session):
             "username": username,
             "password": password,
         }
-        response = requests.post(self.OPENID_CONNECT, data=data, timeout=15)
+        headers = {
+            "User-Agent": user_agent,
+        }
+
+        response = requests.post(self.OPENID_CONNECT, data=data, headers=headers, timeout=15)
         return response.json()["access_token"]
