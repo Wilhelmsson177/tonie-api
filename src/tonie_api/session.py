7a9c806dfdf3d9ad9699b4ae51f5d6370a72abd6
Seji64 <seji@tihoda.de>
2024-09-03 15:37:45 +0200

    make timeout configureable and try to handle timeout a bit better

--- src/tonie_api/session.py
+++ src/tonie_api/session.py
@@ -1,5 +1,6 @@
 """The module of the Toniecloud session."""
 import requests
+from requests.exceptions import Timeout, RequestException
 
 
 class TonieCloudSession(requests.Session):
@@ -13,16 +14,17 @@ class TonieCloudSession(requests.Session):
         super().__init__()
         self.token: None | str = None
 
-    def acquire_token(self, username: str, password: str) -> None:
+    def acquire_token(self, username: str, password: str, timeout: int = 30) -> None:
         """Acquire the token from the ToniCloud SSO login using username and password.
 
         Args:
             username (str): The username
             password (str): The password_
+            timeout (int): The request timeout. Try to increase this value if you receive a timeout error
         """
-        self.token = self._acquire_token(username, password)
+        self.token = self._acquire_token(username, password,timeout)
 
-    def _acquire_token(self, username: str, password: str) -> str:
+    def _acquire_token(self, username: str, password: str, timeout: int) -> str | None:
         data = {
             "grant_type": "password",
             "client_id": "my-tonies",
@@ -30,5 +32,12 @@ class TonieCloudSession(requests.Session):
             "username": username,
             "password": password,
         }
-        response = requests.post(self.OPENID_CONNECT, data=data, timeout=30)
-        return response.json()["access_token"]
+        try:
+            response = requests.post(self.OPENID_CONNECT, data=data, timeout=timeout)
+            response.raise_for_status()
+            return response.json().get("access_token")
+        except Timeout:
+            print(f"Request to acquire token timed out.")
+        except RequestException as e:
+            print(f"An error occurred while acquiring the token: {e}")
+        return None
